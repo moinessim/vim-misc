c88a5e6f49f04b3b3a6c71902e366375ca17ec7b
Moises Nessim <moises.nessim@topmanage.com>
2024-08-27 10:09:03 -0500

    Configure DAP UI

--- lua/vim-misc.lua
+++ lua/vim-misc.lua
@@ -214,3 +214,32 @@ vim.keymap.set("n", "<leader>dl", function() require("dap").set_breakpoint(nil, 
 vim.keymap.set("n", "<leader>dr", function() require("dap").repl.open() end)
 vim.keymap.set("n", "<leader>dk", function() require("dap").run_last() end)
 
+
+---------------------------------------------------------------------
+-- DAP UI
+---------------------------------------------------------------------
+
+local dap =  require("dap")
+local ui = require("dapui")
+
+ui.setup()
+
+require("nvim-dap-virtual-text").setup()
+
+dap.listeners.before.attach.dapui_config = function()
+  ui.open()
+end
+
+dap.listeners.before.launch.dapui_config = function()
+  ui.open()
+end
+
+dap.listeners.before.event_terminated.dapui_config = function()
+  ui.close()
+end
+
+dap.listeners.before.event_exited.dapui_config = function()
+  ui.close()
+end
+
+
