ef7f255e68cd31bc571dc8b1c20f7d5c2552c47a
Moises Nessim <moises.nessim@topmanage.com>
2025-04-07 18:10:22 -0500

    Fixdeprecated trouble.open_with_trouble

--- lua/vim-misc.lua
+++ lua/vim-misc.lua
@@ -37,15 +37,15 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-local trouble = require("trouble.providers.telescope")
+local trouble = require("trouble.sources.telescope")
 
 local telescope = require("telescope")
 
 telescope.setup {
   defaults = {
     mappings = {
-      i = { ["<c-t>"] = trouble.open_with_trouble },
-      n = { ["<c-t>"] = trouble.open_with_trouble },
+      i = { ["<c-t>"] = trouble.open },
+      n = { ["<c-t>"] = trouble.open },
     },
   },
 }
